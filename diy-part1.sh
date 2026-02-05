6bf13398aea2afee2cf3b2039754c0e9fe86e499
sunlt77 <103977593+sunlt77@users.noreply.github.com>
2026-02-05 13:14:28 +0800

    Update feeds and clone passwall package
    Remove outdated luci version and clone updated passwall package.

--- diy-part1.sh
+++ diy-part1.sh
@@ -6,8 +6,11 @@
 # Add a feed source
 #sed -i '$a src-git helloworld https://github.com/fw876/helloworld' ./feeds.conf.default
 #sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' ./feeds.conf.default
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
+#sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' ./feeds.conf.default
 #sed -i '$a src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main' ./feeds.conf.default
 
-# homeproxy
-#git clone --depth=1 https://github.com/immortalwrt/homeproxy.git package/homeproxy
+
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/Openwrt-Passwall/openwrt-passwall package/passwall-luci
